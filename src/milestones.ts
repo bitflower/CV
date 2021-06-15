ce6a91f260c7ec9688739488b2b818884ec611b8
bitflower <matthias.max@bitflower.net>
2021-06-15 20:46:56 +0200

    .

---
+++ src/milestones.ts
@@ -0,0 +1,34 @@
+export const milestones = [
+  {
+    label: "1999 - 2001",
+    content: "Schüler, Softwareentwickler",
+  },
+  {
+    label: "2000 – 2006",
+    content: "EDM-Artist & Musikproduzent - Mind One",
+  },
+  {
+    label: "2003 - 2006",
+    content: "Musikverleger & Plattenlabel - Breeze Records",
+  },
+  {
+    label: "2002 - 2006",
+    content: "Studium Wirtschaftsinformatik - Fachhochschule Pforzheim",
+  },
+  {
+    label: "2006",
+    content: "Diplom Wirtschaftsinformatik (FH) - TITEL DIPLOMARBEIT",
+  },
+  {
+    label: "2006 - 2010",
+    content: "Freelance IT-Consultant - diverse Kunden",
+  },
+  {
+    label: "2010 – 2014",
+    content: "Songwriter und Autor - BMG Rights Management Berlin",
+  },
+  {
+    label: "seit 2014",
+    content: "Geschäftsführer – bitflower",
+  },
+];
