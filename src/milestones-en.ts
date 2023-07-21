9804b15dfcb4679ad9dced94b366f7cd4c03cdbc
Matthias Max <matthias.max@bitflower.net>
2023-07-21 14:52:52 +0200

    feat(): english milestones translations

--- src/milestones-en.ts
+++ src/milestones-en.ts
@@ -1,39 +1,40 @@
 export const milestones = [
   {
     label: '1999 - 2001',
-    content: 'Schüler, Softwareentwickler',
+    content: 'Student, Software Developer',
   },
   {
     label: '2000 – 2006',
-    content: 'EDM-Artist & Musikproduzent - Mind One',
+    content: 'EDM artist & Music Producer - Mind One',
   },
   {
     label: '2003 - 2006',
-    content: 'Musikverleger & Plattenlabelinhaber - Breeze Records',
+    content: 'Music Publisher & Record Label Owner - Breeze Records',
   },
   {
     label: '2001 - 2006',
-    content: 'Studium Wirtschaftsinformatik - Fachhochschule Pforzheim',
+    content: 'Business IT Studies - Pforzheim University of Applied Sciences',
   },
   {
     label: '2006',
     content:
-      'Diplom Wirtschaftsinformatik (FH) - Impactanalyse von Systemausfällen auf Service Level Agreements mit 3D Visualisierung in Java',
+      'Degree in Business IT (FH) - Impact Analysis of System Failures on Service Level Agreements with 3D Visualization in Java',
   },
   {
     label: '2002 - 2010',
-    content: 'Freelance IT-Consultant - Konzern- und Mittelstandsberatung',
+    content:
+      'Freelance IT-Consultant - Corporate and Medium-sized Business Consulting',
   },
   {
     label: '2010 – 2014',
-    content: 'Songwriter und Autor - BMG Rights Management Berlin',
+    content: 'Songwriter and Author - BMG Rights Management Berlin',
   },
   {
-    label: 'seit 2014',
-    content: 'Geschäftsführer – bitflower',
+    label: 'since 2014',
+    content: 'CEO – bitflower',
   },
   {
-    label: 'seit 2019',
-    content: 'Gründer – CaseOS Low-Code Plattform',
+    label: 'since 2019',
+    content: 'Founder – CaseOS Low-Code Platform',
   },
 ];
